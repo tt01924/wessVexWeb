8ce7456021dd36b69956cae7cc0fb6cceb1dcd68
Todd <toddtaylor189@gmail.com>
2024-06-23 17:03:26 +0100

    various little adjustments

--- public/contact/contact.js
+++ public/contact/contact.js
@@ -1,3 +1,5 @@
-document.getElementById('return').onclick = function () {
-    window.location.href = '../index/index.html';
-};
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('return').onclick = function () {
+        window.location.href = '../index/index.html';
+    }
+});
